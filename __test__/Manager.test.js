34140d3ddb2bff1cd93f3f1db42076af94e04b24
lrpineda <luicks212@gmail.com>
2022-05-25 00:17:44 -0600

    Finishing manager test suite

---
+++ __test__/Manager.test.js
@@ -0,0 +1,17 @@
+const Manager = require('../lib/Manager');
+
+test('create the manager object', () => {
+    const manager = new Manager('John Smith', 'jsmith@test.com', '1', '800-555-5555');
+
+    expect(manager.name).toBe('John Smith');
+    expect(manager.email).toBe('jsmith@test.com');
+    expect(manager.id).toBe('1');
+    expect(manager.officeNumber).toBe('800-555-5555');
+
+});
+
+test("Get manager's role", () => {
+    const manager = new Manager('John Smith', 'jsmith@test.com', '1', '800-555-5555');
+
+    expect(manager.getRole()).toBe('Manager');
+});
